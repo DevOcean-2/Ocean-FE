1076488d07cb96fe19d840aa5ec57c237d990fe0
jongdae <asdfg9377@naver.com>
2024-10-23 02:02:52 +0900

    style: 하단 바 색상 변경

--- app/+html.tsx
+++ app/+html.tsx
@@ -41,7 +41,7 @@ export default function Root({ children }: { children: React.ReactNode }) {
 
 const responsiveBackground = `
 body {
-  background-color: #fff;
+  background-color: #EDF1F7;
 }
 @media (prefers-color-scheme: dark) {
   body {
