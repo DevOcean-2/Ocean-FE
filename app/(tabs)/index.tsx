14f1cc2648f1167088f2fd4d1055bf708f8e76fb
jongdae <asdfg9377@naver.com>
2024-10-23 02:03:18 +0900

    feat: home component는 산책 page redirect 하도록 설정

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,5 @@
-import { StyleSheet } from 'react-native';
+import { Redirect } from 'expo-router';
 
-import { Button, Text, View } from 'react-native-ui-lib';
-
-const Home = () => {
-  return (
-    <View flex padding-page>
-      <Text heading>Welcome to My App</Text>
-      <Button label="Press me" onPress={() => console.log('Button pressed')} />
-    </View>
-  );
-};
-
-export default Home;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+export default function Index() {
+  return <Redirect href="/home" />;
+}
