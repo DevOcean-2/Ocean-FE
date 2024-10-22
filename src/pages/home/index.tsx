25fa5efb25ad34c8d0e5576abfa0a922ab58d0bb
jongdae <asdfg9377@naver.com>
2024-10-23 02:09:28 +0900

    style: container style

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,3 @@
-import { SwitchButton } from '@/components/SwitchButton';
-import { useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { View } from 'react-native-ui-lib';
 import { Home } from './Home';
@@ -11,35 +9,15 @@ export const WalkPage = () => {
    * home, 산책하기 메뉴 이동
    */
   const { isHome } = useLocalSearchParams();
-
-  return (
-    <View
-      style={{
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 16,
-      }}
-    >
-      {isHome === 'true' ? <Home /> : <Walk />}
-    </View>
-  );
+  return <View style={styles.container}>{isHome === 'true' ? <Home /> : <Walk />}</View>;
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 16,
   },
 });
