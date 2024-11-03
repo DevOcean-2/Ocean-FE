7d8fad3acefe7de88e8f4ae47d56bf1e5359d455
jyh4479 <jyh.dev@gmail.com>
2024-11-03 15:30:26 +0900

    feat: Feed layout & page entry

---
+++ app/(tabs)/feed/feed-like-list.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+const FeedLikeList = () => {
+  return (
+    <View>
+      <Text>Feed 좋아요 목록</Text>
+    </View>
+  );
+};
+
+export default FeedLikeList;
--- app/(tabs)/my.tsx
+++ app/(tabs)/my.tsx
@@ -1,9 +1,31 @@
 import { Text, View } from 'react-native';
+import { Button } from 'react-native-ui-lib';
+import { Link, useNavigation } from 'expo-router';
 
 const My = () => {
+  const navigation = useNavigation();
+
   return (
     <View>
       <Text>My</Text>
+      <Link href={'/feed/feed-other'} asChild>
+        <Button>
+          <Text>go other</Text>
+        </Button>
+      </Link>
+      <Link href={'/feed/feed-other'} asChild>
+        <Button>
+          <Text>go other</Text>
+        </Button>
+      </Link>
+      <Link href={'/feed/feed-other'} asChild>
+        <Button>
+          <Text>go other</Text>
+        </Button>
+      </Link>
+      <Button onPress={() => navigation.goBack()}>
+        <Text>Back</Text>
+      </Button>
     </View>
   );
 };
