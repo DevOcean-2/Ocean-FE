ef2dd365cee2d86f3ecd338969b81e3915aa4229
jongdae <asdfg9377@naver.com>
2024-11-06 20:22:00 +0900

    feat: fire_white icon

--- assets/svgs/index.ts
+++ assets/svgs/index.ts
@@ -15,3 +15,4 @@ export { default as ICON_RANKING } from './ranking.svg';
 export { default as ICON_FLAG } from './flag.svg';
 export { default as ICON_FIRE } from './fire.svg';
 export { default as ICON_CHECK } from './check.svg';
+export { default as ICON_FIRE_WALK } from './fire_white.svg';
