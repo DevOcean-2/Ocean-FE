4a9032e967bf6fdb5f34ab5c427af9aef9b7a596
JIHOON LEE <jihoon7705@gmail.com>
2024-11-20 19:41:57 +0900

    feature : 장소 페이지 지도 생성

--- app/(tabs)/place.tsx
+++ app/(tabs)/place.tsx
@@ -1,9 +1,11 @@
+import PlaceComponent from '@/src/pages/Place';
 import { View, Text } from 'react-native';
 
 const Place = () => {
   return(
-    <View>
+    <View style={{width : '100%', height: "100%", backgroundColor:"white"}}>
       <Text>Three</Text>
+      <PlaceComponent />
     </View>
   )
 }
